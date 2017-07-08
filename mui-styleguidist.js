a823c96ad32faf13821fe3449621cbdd34b00830
leMaik <an@maik.tk>
2017-07-08 18:37:13 +0200

    Fix binary path when styleguide is not installed inside the wrapper's node_modules directory.

--- mui-styleguidist.js
+++ mui-styleguidist.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 const { spawnSync } = require('child_process')
 const path = require('path')
-require.resolve('react-styleguidist')
 
 spawnSync(
-    path.join(__dirname, 'node_modules', '.bin', 'styleguidist'),
-    [
-        '--config', path.join(__dirname, 'styleguide.config.js'),
-        ...process.argv.slice(2)
-    ],
-    { stdio: 'inherit', cwd: process.cwd() }
+  require.resolve('react-styleguidist/bin/styleguidist'),
+  [
+    '--config', path.join(__dirname, 'styleguide.config.js'),
+    ...process.argv.slice(2)
+  ],
+  { stdio: 'inherit', cwd: process.cwd() }
 )
